76dd04fae9730bc4a9fec23a05f9977d1c1b1e78
Trang <thutrangpham1509@gmail.com>
2022-08-17 21:41:01 +0700

    deploy db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('qlnh', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
+const sequelize = new Sequelize('db2soukmu4ieub', 'caneaomujeynjp', "f92d7caee605fd211c88039ea150437c2704d95d70423213a01e9daad0f51e82", {
+  host: 'ec2-34-203-182-65.compute-1.amazonaws.com',
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 let connectDB = async () => {
